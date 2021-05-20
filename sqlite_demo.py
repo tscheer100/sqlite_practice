d4dad90859f07202608c71dbeb4223560a7f4d4a
tscheer100 <thomas.k.scheer@gmail.com>
2021-05-20 16:16:25 -0500

    wrote couple of commands, manually added data to db

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -5,14 +5,22 @@ conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
 
-c.execute("""
-CREATE TABLE employyes (
-    first text,
-    last text,
-    pay integer
-)
- """)
+# c.execute("""
+# CREATE TABLE employees (
+#     first text,
+#     last text,
+#     pay integer
+# )
+#  """)
  # datatypes can bee found here: https://www.sqlite.org/datatype3.html
+
+# c.execute("INSERT INTO employees VALUES ('Thomas', 'Scheer', 50000)")
+# c.execute("INSERT INTO employees VALUES ('Diana', 'Scheer', 70000)")
+# conn.commit()
+
+c.execute("SELECT * FROM employees WHERE last='Scheer'")
+
+print(c.fetchall())
 
 conn.commit()
 
